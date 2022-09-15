63e88e65be4e1e9d01ed157710abddad0ffeb318
yonasFeshaye <wedimokie2012@gmail.com>
2022-09-14 21:42:02 -0400

    Updated the HTML, CSS and JavaScript files.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let character = []
+let button = document.getElementById("btn")
+let inputs = document.getElementById("input1")
 
+button.addEventListener("click", function(){
+   let randomCharacter = Math.floor(Math.random() * characters.length)
+   console.log(randomCharacter)
+   for(let i = 0; i < characters.length; i ++) {
+       inputs.value += characters[i] 
+       
+   }
+})
 
 
 
